c2df2666ec626ea68b95a525b081e7b3b0310c68
Bruno Urbano <bruno.urbano.rocha@gmail.com>
2021-08-01 15:28:47 -0300

    created projetos models + urls + folder templates

--- projetos/admin.py
+++ projetos/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import TipoTarefa, PrioridadeTarefa, Tarefa
 
-# Register your models here.
+
+class TipoTarefaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'descricao')
+    list_editable = ('descricao',)
+
+
+class PrioridadeTarefaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'descricao')
+    list_editable = ('descricao',)
+
+
+class TarefaAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'titulo',
+        'descricao',
+        'tipo',
+        'prioridade',
+        'data_criacao',
+        'data_final'
+    )
+    list_editable = (
+        'titulo',
+        'descricao',
+        'tipo',
+        'prioridade',
+        'data_final'
+    )
+
+
+admin.site.register(TipoTarefa, TipoTarefaAdmin)
+admin.site.register(PrioridadeTarefa, PrioridadeTarefaAdmin)
+admin.site.register(Tarefa, TarefaAdmin)
--- projetos/models.py
+++ projetos/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class TipoTarefa(models.Model):
+    # Exemplo: pode ser um evento, uma tarefa, um lembrete, etc
+    # Ja deixar criado
+    descricao = models.CharField()
+
+    class Meta:
+        verbose_name = 'Tipo'
+        verbose_name_plural = 'Tipos'
+
+
+class PrioridadeTarefa(models.Model):
+    # Exemplo: alta, média, baixa
+    # Ja deixar criado
+    descricao = models.CharField()
+
+    class Meta:
+        verbose_name = 'Prioridade'
+        verbose_name_plural = 'Prioridades'
+
+
+class Tarefa(models.Model):
+    titulo = models.CharField()
+    descricao = models.TextField(max_length=255)
+    tipo = models.ForeignKey(TipoTarefa, blank=True, null=True, on_delete=models.DO_NOTHING)
+    prioridade = models.ForeignKey(PrioridadeTarefa, blank=True, null=True, on_delete=models.DO_NOTHING)
+    data_criacao = models.DateTimeField(default=datetime.now(), editable=False)
+    data_final = models.DateTimeField()
+
+    verbose_name = 'Tarefa'
+    verbose_name_plural = 'Tarefas'
+
+    def __str__(self):
+        return f'{self.titulo}'
