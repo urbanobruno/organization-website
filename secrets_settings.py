c81b74eaf583726cc3fa002ef59c877ad3794f3c
Bruno Urbano <bruno.urbano.rocha@gmail.com>
2021-08-02 13:52:08 -0300

    Untrack files in .gitignore fix

--- secrets_settings.py
+++
@@ -1,8 +0,0 @@
-
-dados = dict()
-
-dados['SECRET_KEY'] = 'django-insecure-m2l101v=qo4(80bs2+$dvp4pf2sb!dz9g&e5nt417)@rwd=2x1'
-dados['DB_HOST'] = '127.0.0.1'
-dados['DB_NAME'] = 'to_do_list'
-dados['DB_USER'] = 'root'
-dados['DB_PORT'] = '3306'
